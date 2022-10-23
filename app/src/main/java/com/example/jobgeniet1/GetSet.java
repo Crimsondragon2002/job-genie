51ddeca919de261be144fa9a40a0089f47ba1327
Crimsondragon2002 <91789792+Crimsondragon2002@users.noreply.github.com>
2022-10-22 20:51:51 -0400

    Update GetSet.java
    As I said in comments, this entire class is just to make an exterieor functions for values that make the fragment movement go smoothly between the second to fourth, and  third to fourth, and vice verca.

--- app/src/main/java/com/example/jobgeniet1/GetSet.java
+++ app/src/main/java/com/example/jobgeniet1/GetSet.java
@@ -1,10 +1,13 @@
 package com.example.jobgeniet1;
-
+//this entie section is just extra code to make sure that going back and fourth between trying to change data goes to the proper fragment
 public class GetSet {
+    
     private static boolean Seconding = false;
+    //gets the boolean seconding, which checks to see if it goes through the second fragment
     public boolean getSeconding(){
         return Seconding;
     }
+    //sets the boolean seconding, which checks to see if it goes through the second fragment
     void setSeconding(boolean Second){
         this.Seconding = Second;
     }
